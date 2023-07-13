aed1fef995f6b90514659ce774cfa9bfa1d98c17
ervinMamutov <ervin.news@gmail.com>
2023-07-13 13:28:49 +0200

    finish arrays-of-things

--- 1-remix/using-errors/exercises/2-expect-to-throw/arrays-of-things/find-smallest-number.js
+++ 1-remix/using-errors/exercises/2-expect-to-throw/arrays-of-things/find-smallest-number.js
@@ -1,11 +1,17 @@
-// #todo
-
 // you can use this to check the argument
-import { isArrayOfNumbers } from './is-array-of-numbers.js';
+import { isArrayOfNumbers } from './utils/is-array-of-numbers.js';
 
 /**
  *
  * @param {*} numbers
  * @returns
  */
-export const findSmallestNumber = (numbers = []) => {};
+export const findSmallestNumber = (numbers = []) => {
+  if (!isArrayOfNumbers(numbers)) {
+    throw new TypeError('The array is non a number');
+  }
+
+  if (numbers.length === 0) return;
+
+  return numbers.sort()[0];
+};
